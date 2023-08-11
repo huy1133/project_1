62e1a044edb5b711af29978c4ef41c0e44e7d233
tranvanhuy <vanhuy26032003@gmail.com>
2023-08-11 17:30:59 +0700

    add

---
+++ Assets/scripts/playerControler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+ 
+    public float speed;
+    public Vector3[] point;
+    public int current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = point[current]-transform.position;
+        if (direction.magnitude > 0.1f)
+        {
+            transform.forward = direction.normalized;
+            transform.position +=transform.forward * speed *Time.deltaTime;
+        }
+        else
+        {
+            current++;
+            if(current == 4)
+                current = 0;
+        }
+    }
+    
+}
